2aa8cec409aac4e7192bc492436f9e81e9edf230
showrya <kollushowrya@gmail.com>
2018-12-05 13:36:00 -0500

    commit 12/05 -13:34
    worked on captureing the changes with property setter, ngOnChanges, Input and Output properties and parent and child component commuication.

--- src/app/display-employee/display-employee.component.ts
+++ src/app/display-employee/display-employee.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit,Input } from '@angular/core';
+import { Component, OnInit, Input,Output,OnChanges, SimpleChanges, SimpleChange, EventEmitter } from '@angular/core';
 import { employee } from '../models/employee.model';
 
 @Component({
@@ -6,11 +6,49 @@ import { employee } from '../models/employee.model';
   templateUrl: './display-employee.component.html',
   styleUrls: ['./display-employee.component.css']
 })
-export class DisplayEmployeeComponent implements OnInit {
-@Input() employee: employee;
-  constructor() { }
+export class DisplayEmployeeComponent implements OnInit { //, OnChanges
+  // @Input() employeeId : number;
+  @Input() employee : employee;
+
+  @Output() 
+  notify: EventEmitter<employee> = new EventEmitter<employee>();
+  //  @Input() 
+  // employee: employee;
+  //   constructor() { } // this code is whe we are using onchanges below is for if we use getter and setter to log changes
+
+  // private _employee: employee;
+  // @Input()
+  // set employee(val: employee) {
+  //   // console.log('previous employee:' + (this._employee ? this._employee.name :'null'));
+  //   // console.log('current employee:' + val.name);
+  //   this._employee = val;
+
+
+  // }
+
+  // get employee(): employee {
+  //   return this._employee
+
+
+  // }
+
 
   ngOnInit() {
   }
+
+  handleClick(){
+    this.notify.emit(this.employee);
+    
+
+  }
+
+  // ngOnChanges(changes:SimpleChanges){
+
+  //   const previousEMployee = <employee> changes.employee.previousValue;
+  //   const currentEmployee = <employee> changes.employee.currentValue;
+  //   console.log('previous employee:' + (previousEMployee ? previousEMployee.name :'null'));
+  //   console.log('current employee:' + currentEmployee.name);
+
+  // }
 
 }
--- src/app/employee/list-employees.component.ts
+++ src/app/employee/list-employees.component.ts
@@ -9,10 +9,26 @@ import { EmployeeService } from './employee.service';
 export class ListEmployeesComponent implements OnInit {
 
   employees: employee[];
+  dataFromChild: employee;
+  // employeeToDisplay:employee;
+  // private arrayIndex = 1;
   constructor(private _employeeService: EmployeeService) { }
 
   ngOnInit() {
     this.employees = this._employeeService.getEmployees();
+    // this.employeeToDisplay = this.employees[0];
   }
-
+  handleNotify(eventData: employee){
+    this.dataFromChild = eventData;
+  }
+  // nextEmployee():void{
+  //   console.log("clicked display next employee button");
+  //   if (this.arrayIndex <= 2){
+  //   this.employeeToDisplay = this.employees[this.arrayIndex];
+  //   this.arrayIndex++}
+  //   else{
+  //     this.employeeToDisplay = this.employees[0];
+  //     this.arrayIndex = 1;
+  //   }
+  // }
 }
