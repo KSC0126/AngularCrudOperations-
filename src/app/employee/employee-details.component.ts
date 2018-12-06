01a96c120432ad77bc5c7edd55709d99e84b56ce
showrya <kollushowrya@gmail.com>
2018-12-05 22:44:29 -0500

    commit 12/05 22:42
    router parameter with subscribe in angular rather than params and snapshot

--- src/app/employee/employee-details.component.ts
+++ src/app/employee/employee-details.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { ActivatedRoute } from '@angular/router';
+import { ActivatedRoute, Router } from '@angular/router';
 
 import { EmployeeService } from './employee.service';
 import { employee } from '../models/employee.model';
@@ -11,14 +11,32 @@ import { employee } from '../models/employee.model';
 })
 export class EmployeeDetailsComponent implements OnInit {
   employee: employee;
-  
+  private _id: number;
 
-  constructor( private _route : ActivatedRoute,
-   private _EmployeeService: EmployeeService){ }
+
+  constructor(private _route: ActivatedRoute,
+    private _EmployeeService: EmployeeService,
+    private _router: Router) { }
 
   ngOnInit() {
-    const id = +this._route.snapshot.paramMap.get('id');
-    this.employee=this._EmployeeService.getEmployee(id);
+    // this._id = +this._route.snapshot.paramMap.get('id'); this is used for snapshot not for subscribe 
+    // this.employee=this._EmployeeService.getEmployee(this._id);
+    this._route.paramMap.subscribe(parmas => {
+      this._id = +parmas.get('id');
+      this.employee = this._EmployeeService.getEmployee(this._id);
+
+    })
+  }
+  viewNextEmployee() {
+    if (this._id < 3) {
+      this._id = this._id + 1;
+
+    }
+    else {
+      this._id = 1;
+    }
+    
+    this._router.navigate(['/employee', this._id])
   }
 
 }
