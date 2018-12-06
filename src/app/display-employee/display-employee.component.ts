3d8dc27d443b1ec3367f5ba0d1baf7ae93a6114c
showrya <kollushowrya@gmail.com>
2018-12-06 05:21:50 -0500

    commit-12/06 5:21
    used optional routing parameter and captured them to make changes on the view with captured data

--- src/app/display-employee/display-employee.component.ts
+++ src/app/display-employee/display-employee.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit, Input,Output,OnChanges, SimpleChanges, SimpleChange, EventEmitter } from '@angular/core';
 import { employee } from '../models/employee.model';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-display-employee',
@@ -7,50 +8,19 @@ import { employee } from '../models/employee.model';
   styleUrls: ['./display-employee.component.css']
 })
 export class DisplayEmployeeComponent implements OnInit { //, OnChanges
-  // @Input() employeeId : number;
+ 
+  private selectedEmployeeId: number;
   @Input() employee : employee;
 
-  // @Output() 
-  // notify: EventEmitter<employee> = new EventEmitter<employee>();
-  //  @Input() 
-  // employee: employee;
-  //   constructor() { } // this code is whe we are using onchanges below is for if we use getter and setter to log changes
+ 
+    constructor( private _route : ActivatedRoute) { } // this code is whe we are using onchanges below is for if we use getter and setter to log changes
 
-  // private _employee: employee;
-  // @Input()
-  // set employee(val: employee) {
-  //   // console.log('previous employee:' + (this._employee ? this._employee.name :'null'));
-  //   // console.log('current employee:' + val.name);
-  //   this._employee = val;
-
-getEmployeeFromChild(){
-  return this.employee.name + '' + this.employee.gender;
-}
-  // }
-
-  // get employee(): employee {
-  //   return this._employee
-
-
-  // }
-
+  
 
   ngOnInit() {
+    this.selectedEmployeeId = +this._route.snapshot.paramMap.get('id');
   }
 
-  // handleClick(){
-  //   this.notify.emit(this.employee);
-    
-
-  // }
-
-  // ngOnChanges(changes:SimpleChanges){
-
-  //   const previousEMployee = <employee> changes.employee.previousValue;
-  //   const currentEmployee = <employee> changes.employee.currentValue;
-  //   console.log('previous employee:' + (previousEMployee ? previousEMployee.name :'null'));
-  //   console.log('current employee:' + currentEmployee.name);
-
-  // }
+  
 
 }
